61bff631daa9dd701c2d9370e3d73d2865b59664
Mostafa Hafezypoor <46339325+mostafaHafezyPoor20@users.noreply.github.com>
2026-07-24 20:24:32 +0330

    Update Constants.java

--- app/src/main/java/mostafa/hafezypoor/shikshap/utils/Constants.java
+++ app/src/main/java/mostafa/hafezypoor/shikshap/utils/Constants.java
@@ -2,7 +2,6 @@ package mostafa.hafezypoor.shikshap.utils;
 
 public class Constants {
     //public static String BASE_URL="http://192.168.1.5/shikShapAndroidClient/";
-    public static String BASE_URL="https://shikshap.ir/shikShapAndroidClient/";
     public static String URL_SHARE_PRODUCT="https://shikshap.ir/product.php";
     public static String VERSION="1";
 }
